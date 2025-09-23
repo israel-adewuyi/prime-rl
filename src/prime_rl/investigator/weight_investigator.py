00f60f850593eec3a3257c5627ac235054178ed4
Israel Adewuyi <isistickz@gmail.com>
2025-09-23 03:09:44 +0000

    Implemented checking for 1-1 mapping between models

--- src/prime_rl/investigator/weight_investigator.py
+++ src/prime_rl/investigator/weight_investigator.py
@@ -25,79 +25,31 @@ class WeightInvestigator:
         self.logger.info(f"Loading second model at path {config.checkpoint_path_2}")
         self.model_2 = AutoModelForCausalLM.from_pretrained(config.checkpoint_path_2)
 
+        self._check_one_to_one_mapping()
+
         self.logger.info("Generating stats")
-        self.generate_stats()
+        # self.generate_stats()
 
-    def generate_stats(self) -> None:
-        stats = {}
-        tok_embeds = self.get_token_embed()
-        diff_tensor = tok_embeds[0] - tok_embeds[1]
-        stats["token_embedding"] = diff_tensor.abs().mean().item()
-        self.logger.info(f"Difference in token embedding is {diff_tensor.abs().mean()}")
+    def _check_one_to_one_mapping():
+        """This function checks if both models have a 1-1 mappping. 
+           This is achieved by checking if the set of modules in both models are the same
+        """
+        set_model_a = set(self.model_1.state_dict().keys())
+        set_model_b = set(self.model_2.state_dict().keys())
 
-        # stats["attn"] = []
-        # for layer in range(self.model_1.config.num_hidden_layers):
-        #     layer_diff = self.get_attn_diff_at_layer(layer)
-        #     stats["attn"].append(layer_diff)
-        #     self.logger.info(f"Difference in attention at layer {layer} is {layer_diff}")
-
-        # stats["mlp"] = []
-        # for layer in range(self.model_1.config.num_hidden_layers):
-        #     layer_diff = self.get_mlp_diff_at_layer(layer)
-        #     stats["mlp"].append(layer_diff)
-        #     self.logger.info(f"Difference in MLP at layer {layer} is {layer_diff}")
-
-        plot_weight_diffs(stats, self.config.output_dir / "weight_diff.html")
-
-    def get_token_embed(self) -> Tuple[Tensor, Tensor]:
-        # Get the token embedding layer 
-        tok_embed_1 = self.model_1.model.embed_tokens.weight.data
-        tok_embed_2 = self.model_2.model.embed_tokens.weight.data
-
-        return (tok_embed_1, tok_embed_2)
-
-    def get_token_umembed(self) -> Tuple[Tensor, Tensor]:
-        # Get the token unembedding layer
-        if model.config.tie_word_embeddings:
-            tok_unembed_1 = self.model_1.model.embed_tokens.weight.data.T
-            tok_unembed_2 = self.model_2.model.embed_tokens.weight.data.T
+        if set_model_a != set_model_b:
+            missing_in_a = set_model_b - set_model_a
+            missing_in_b = set_model_a - set_model_b
+            raise ValueError(
+                f"Model state_dict mismatch.\n"
+                f"Missing in model_1: {missing_in_a}\n"
+                f"Missing in model_2: {missing_in_b}"
+            )
         else:
-            tok_unembed_1 = self.model_1.model.unembed_tokens.weight.data
-            tok_unembed_2 = self.model_2.model.unembed_tokens.weight.data
+            self.logger.info(f"All modules align in both models")
+        
 
-        return (tok_unembed_1, tok_unembed_2)
-
-    def get_attn(self) -> Tuple[Tensor, Tensor]:
-        pass
-
-    def get_attn_diff_at_layer(self, layer: int) -> float:
-        attn_1 = self.model_1.model.layers[layer].self_attn.state_dict()
-        attn_2 = self.model_2.model.layers[layer].self_attn.state_dict()
-
-        stats = []
-        for key in attn_1:
-            if key in attn_2:
-                diff = (attn_1[key] - attn_2[key]).abs().mean()
-                self.logger.debug(f"Data type of diff in get_attn_at_layer is {type(diff)}")
-                stats.append(diff.item())
-
-        return np.mean(np.array(stats))
-
-    def get_mlp(self) -> Tuple[Tensor, Tensor]:
-        pass
-
-    def get_mlp_diff_at_layer(self, layer: int) -> float:
-        mlp_1 = self.model_1.model.layers[layer].mlp.state_dict()
-        mlp_2 = self.model_2.model.layers[layer].mlp.state_dict()
-
-        stats = []
-        for key in mlp_1:
-            if key in mlp_2:
-                diff = (mlp_1[key] - mlp_2[key]).abs().mean()
-                self.logger.debug(f"Data type of diff in get_mlp_diff_at_layer is {type(diff)}")
-                stats.append(diff.item())
-
-        return np.mean(np.array(stats))
+    
 
 def main():
     """Main entry-point for investigator. Run using `uv run investigator`"""
