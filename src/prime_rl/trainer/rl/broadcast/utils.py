36b5dd94af7b27a99e7dc0c6bf1e02185a7d5d0d
samsja <55492238+samsja@users.noreply.github.com>
2025-11-04 19:28:56 -0800

    more performant nccl broadcast (#1227)
    * wip
    
    * maybe fix mismatch
    
    * add back skip
    
    * add bucketed send recv
    
    * add back skip

--- src/prime_rl/trainer/rl/broadcast/utils.py
+++ src/prime_rl/trainer/rl/broadcast/utils.py
@@ -4,9 +4,9 @@ import torch
 
 
 def tensor_string_description(tensor: torch.Tensor) -> bytes:
-    return pickle.dumps((tensor.shape))
+    return pickle.dumps((tensor.shape, tensor.dtype))
 
 
-def init_tensor_from_string_description(description: bytes, device: torch.device, dtype: torch.dtype) -> torch.Tensor:
-    shape = pickle.loads(description)
+def init_tensor_from_string_description(description: bytes, device: torch.device) -> torch.Tensor:
+    shape, dtype = pickle.loads(description)
     return torch.empty(shape, dtype=dtype, device=device)
