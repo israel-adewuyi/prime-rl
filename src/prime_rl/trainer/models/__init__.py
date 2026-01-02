f731eb9d0770b2ccf7b75ac5f1590fadb3dd207d
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2026-01-02 01:21:55 -0800

    feat: Add AFMoE support  (#1515)
    * basic slow afmoe setup
    
    * add flex attention
    
    * avoid compilation error on flex-attn
    
    * super rough fix for swa and sequence packing
    
    * massively simplify impl
    
    * remove a bunch of dead/unused code and clean up
    
    * un-correct unrelated bug for later fix
    
    * remove debug config file accidentally left in
    
    * remove redundant extra imports
    
    * simplify attention impl
    
    * revert comment
    
    * remove flex_attn for now
    
    * fix ruff lints
    
    * do not put attn in separate file
    
    * satisfy ruff

--- src/prime_rl/trainer/models/__init__.py
+++ src/prime_rl/trainer/models/__init__.py
@@ -8,17 +8,20 @@ from transformers.models.auto.auto_factory import _BaseAutoModelClass, _LazyAuto
 from transformers.models.auto.configuration_auto import CONFIG_MAPPING_NAMES
 from transformers.models.llama.configuration_llama import LlamaConfig
 
+from prime_rl.trainer.models.afmoe import AfmoeConfig, AfmoeForCausalLM
 from prime_rl.trainer.models.base import PreTrainedModelPrimeRL
 from prime_rl.trainer.models.glm4_moe import Glm4MoeConfig, Glm4MoeForCausalLM
 from prime_rl.trainer.models.llama import LlamaForCausalLM
 from prime_rl.trainer.models.qwen3_moe import Qwen3MoeConfig, Qwen3MoeForCausalLM
 
 # Make custom config discoverable by AutoConfig
+AutoConfig.register("afmoe", AfmoeConfig, exist_ok=True)
 AutoConfig.register("glm4_moe", Glm4MoeConfig, exist_ok=True)
 AutoConfig.register("qwen3_moe", Qwen3MoeConfig, exist_ok=True)
 
 _CUSTOM_CAUSAL_LM_MAPPING = _LazyAutoMapping(CONFIG_MAPPING_NAMES, OrderedDict())
 _CUSTOM_CAUSAL_LM_MAPPING.register(LlamaConfig, LlamaForCausalLM, exist_ok=True)
+_CUSTOM_CAUSAL_LM_MAPPING.register(AfmoeConfig, AfmoeForCausalLM, exist_ok=True)
 _CUSTOM_CAUSAL_LM_MAPPING.register(Glm4MoeConfig, Glm4MoeForCausalLM, exist_ok=True)
 _CUSTOM_CAUSAL_LM_MAPPING.register(Qwen3MoeConfig, Qwen3MoeForCausalLM, exist_ok=True)
 
