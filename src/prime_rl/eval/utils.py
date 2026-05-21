0319d48798588e5c99f67115aca0fe27f71c5d3f
Israel Adewuyi <isistickz@gmail.com>
2026-05-21 04:38:37 +0300

    iimport dataset

--- src/prime_rl/eval/utils.py
+++ src/prime_rl/eval/utils.py
@@ -7,6 +7,7 @@ from typing import Any
 
 import numpy as np
 import pandas as pd
+from datasets import Dataset
 from huggingface_hub import whoami
 from openai import AsyncOpenAI
 from prime_evals import AsyncEvalsClient
